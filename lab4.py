2e2db50797e3968efff26179c107f60ccb5934a1
Andy Wang <andyjw2@illinois.edu>
2025-02-18 17:52:42 -0500

    add orangesRotting

--- lab4.py
+++ lab4.py
@@ -1,10 +1,9 @@
-
 def numIslands(grid: list[list[str]]) -> int:
     # count connected components
     # doesn't use visited set because modifies grid in-place
     height, width = len(grid), len(grid[0])
     ccs = 0
-    
+
     for r in range(height):
         for c in range(width):
             if grid[r][c] == '1':
@@ -19,8 +18,9 @@ def numIslands(grid: list[list[str]]) -> int:
                         # if ni in range(height) and nj in range(width):
                         if 0 <= ni < height and 0 <= nj < width and grid[ni][nj] == '1':
                             stack.append((ni, nj))
-                            
+
     return ccs
+
 
 def maxAreaOfIsland(grid: list[list[str]]) -> int:
     height, width = len(grid), len(grid[0])
@@ -40,19 +40,20 @@ def maxAreaOfIsland(grid: list[list[str]]) -> int:
                     for ni, nj in [(i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1)]:
                         if ni in range(height) and nj in range(width) and grid[ni][nj] == 1:
                             stack.append((ni, nj))
-                    
+
                 print("Finished island with area", curr_area)
                 max_area = max(max_area, curr_area)
-    
+
     return max_area
 
 
-grid = [
-    [1,1,1,1,0],
-    [1,1,0,1,0],
-    [1,1,0,0,0],
-    [0,0,1,0,1]
-]
+# grid = [
+#     [1, 1, 1, 1, 0],
+#     [1, 1, 0, 1, 0],
+#     [1, 1, 0, 0, 0],
+#     [0, 0, 1, 0, 1]
+# ]
+
 
 def spiralOrder(matrix: list[list[int]]) -> list[int]:
     """strategy:
@@ -73,14 +74,50 @@ def spiralOrder(matrix: list[list[int]]) -> list[int]:
         i, j = curr
         visited.add((i, j))
         out.append(matrix[i][j])
-        
+
         di, dj = dirs[dir_idx]
         if not (i + di in range(m) and j + dj in range(n) and (i + di, j + dj) not in visited):
             dir_idx = (dir_idx + 1) % 4
             di, dj = dirs[dir_idx]
-        
+
         curr = (i + di, j + dj)
-            
+
     return out
-            
-print(spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+
+
+from collections import deque
+def orangesRotting(grid: list[list[int]]) -> int:
+    height, width = len(grid), len(grid[0])
+    fresh_oranges = 0
+    queue = deque()
+    for r in range(height):
+        for c in range(width):
+            if grid[r][c] == 1:
+                fresh_oranges += 1
+            if grid[r][c] == 2:
+                queue.append((r, c, 0))
+
+    if fresh_oranges == 0:
+        return 0
+
+    max_time = 0
+    dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    while queue:
+        i, j, t = queue.popleft()
+        print(f"time {t}:\t{(i, j)}: {grid[i][j]}")
+        for di, dj in dirs:
+            ni, nj = i + di, j + dj
+            if ni in range(height) and nj in range(width) and grid[ni][nj] == 1:
+                grid[ni][nj] = 2
+                queue.append((ni, nj, t + 1))
+                max_time = t + 1
+                fresh_oranges -= 1
+
+    return max_time if fresh_oranges == 0 else -1
+
+
+gr = [[2,1,1,2],[1,1,0,1],[0,1,1,1]]
+# gr = [[0,0]]
+for _ in gr:
+    print(_)
+print(orangesRotting(gr))
