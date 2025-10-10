fb7252565a0aa8fb08467289d8ab0c08ed3eab32
Andy Wang <therainruffy@yahoo.com>
2025-10-10 17:03:44 -0400

    improve twitter problem

--- lab5.py
+++ lab5.py
@@ -143,7 +143,7 @@ def leastInterval2(tasks: List[str], n: int) -> int:
 
 # print(leastInterval2(["A","C","A","B","D","A","B"], 10))
 # print(leastInterval2(["A","A","A","B","B","B"], 2))
-print(leastInterval2(list('SADHFUIHEWIUNNBFDJKHGKLJDSFHFKLJRESHFILUAWEGFBFKDLJBBJNFDJBNSDKFJNBEUIYSORNIUYORHBGIOWAHBGIWAUEHFEIAWUHEFKLJSADHF'), 26))
+# print(leastInterval2(list('SADHFUIHEWIUNNBFDJKHGKLJDSFHFKLJRESHFILUAWEGFBFKDLJBBJNFDJBNSDKFJNBEUIYSORNIUYORHBGIOWAHBGIWAUEHFEIAWUHEFKLJSADHF'), 26))
 
 class Twitter:
 
@@ -155,16 +155,32 @@ class Twitter:
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.tweetMap[userId].append((self.time, tweetId))
         # print(f"adding tweet {tweetId} to user {userId}")
-        self.time +=1
+        self.time -=1  # negate to make minheap work (most recent = latest time = most negative time)
 
     def getNewsFeed(self, userId: int) -> List[int]:
         # print(f"getting news feed for user {userId}: {self.tweetMap}")
-        own_tweets = self.tweetMap[userId][:]
-        following_tweets = [t for other in self.followMap[userId] for t in self.tweetMap[other]]
-        # print(f"\town: {own_tweets} following: {following_tweets}")
-        own_tweets.extend(following_tweets)
-        own_tweets = sorted(own_tweets, key=lambda t: t[0], reverse=True)
-        return [t[1] for t in own_tweets[:10]]
+        heap = []
+        users_to_check = self.followMap[userId].copy()
+        users_to_check.add(userId)
+
+        # heap will be len(num) users- NOT 10
+        # this will become O(log k) where k is # tweets
+        for user in users_to_check:
+            if user in self.tweetMap:
+                tweets = self.tweetMap[user]
+                curr_idx = len(tweets) - 1
+                time, tweet_id = tweets[curr_idx]
+                heapq.heappush(heap, (time, tweet_id, user, curr_idx))
+
+        result = []
+        while heap and len(result) < 10:
+            _, tweet_id, user, last_idx = heapq.heappop(heap)
+            result.append(tweet_id)
+            if last_idx > 0:
+                last_time, last_tweet = self.tweetMap[user][last_idx - 1]
+                heapq.heappush(heap, (last_time, last_tweet, user, last_idx - 1))
+                
+        return result
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.followMap[followerId].add(followeeId)
@@ -174,3 +190,43 @@ class Twitter:
         if followerId in self.followMap:
             self.followMap[followerId].remove(followeeId)
             # print(f"{followerId} unfollowed {followeeId}: map is now {self.followMap}")
+
+def test_twitter():
+    # Test Case 1
+    twitter1 = Twitter()
+    twitter1.postTweet(1, 10)
+    twitter1.postTweet(1, 20)
+    twitter1.postTweet(1, 30)
+    twitter1.follow(2, 1)
+    twitter1.postTweet(2, 15)
+    print("Test 1 - User 2's feed:", twitter1.getNewsFeed(2))
+    twitter1.unfollow(2, 1)
+    print("Test 1 - User 2's feed after unfollow:", twitter1.getNewsFeed(2))
+
+    # Test Case 2
+    twitter2 = Twitter()
+    twitter2.postTweet(5, 505)
+    twitter2.follow(3, 5)
+    twitter2.follow(4, 5)
+    twitter2.postTweet(5, 506)
+    print("Test 2 - User 3's feed:", twitter2.getNewsFeed(3))
+    print("Test 2 - User 4's feed:", twitter2.getNewsFeed(4))
+    twitter2.unfollow(3, 5)
+    print("Test 2 - User 3's feed after unfollow:", twitter2.getNewsFeed(3))
+
+    # Test Case 3: 12 users, each posts one tweet
+    print('=' * 50, "Test 3")
+    twitter3 = Twitter()
+    for user_id in range(1, 13):
+        twitter3.postTweet(user_id, user_id * 100)
+
+        for follower in [1, 5, 12]:
+            for followee in range(1, 13):
+                if followee != follower:
+                    twitter3.follow(follower, followee)
+    twitter3.unfollow(5, 11)
+    twitter3.postTweet(12, 69)
+    for user_id in range(1, 13):
+        print(f"Test 3 - User {user_id}'s feed:", twitter3.getNewsFeed(user_id))
+        
+test_twitter()
