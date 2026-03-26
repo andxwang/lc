4722062235c703613bcd3d384f156d7206c08ec3
Andy Wang <therainruffy@yahoo.com>
2026-03-25 23:22:12 -0400

    write faster readBinaryWatch

--- backtracking.py
+++ backtracking.py
@@ -351,8 +351,24 @@ def readBinaryWatch(turnedOn: int) -> list[str]:
     aux(0, 0)
     return ans
 
-
-# ans = set(readBinaryWatch(2))
-# exp = set(["0:03","0:05","0:06","0:09","0:10","0:12","0:17","0:18","0:20","0:24","0:33","0:34","0:36","0:40","0:48","1:01","1:02","1:04","1:08","1:16","1:32","2:01","2:02","2:04","2:08","2:16","2:32","3:00","4:01","4:02","4:04","4:08","4:16","4:32","5:00","6:00","8:01","8:02","8:04","8:08","8:16","8:32","9:00","10:00"])
-# print(ans - exp)
-# print(exp - ans)
+def readWatchBit(turnedOn: int) -> list[str]:
+    # h: 0-11, m: 0-59
+    ans = []
+    # def num_ones(n: int) -> int:
+    #     """return the number of 1s in the binary representation of n"""
+    #     count = 0
+    #     while n > 0:
+    #         n, r = divmod(n, 2)
+    #         if r == 1:
+    #             count += 1
+    #     return count
+    
+    for h in range(12):
+        h_ones = h.bit_count()
+        if h_ones <= turnedOn:
+            for m in range(60):
+                m_ones = m.bit_count()  # stupid bit trick
+                if h_ones + m_ones == turnedOn:
+                    ans.append(f'{h}:{m:02d}')
+                
+    return ans
